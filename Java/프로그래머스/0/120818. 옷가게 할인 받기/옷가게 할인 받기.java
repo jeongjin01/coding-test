dd5e1b14af5d4548900318b77a71074c70744f5f
JeongJin <124764438+jeongjin01@users.noreply.github.com>
2024-03-28 23:57:21 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int price) {
+        if (500000 <= price) {
+            price *= 0.8;
+        } else if (300000 <= price) {
+            price *= 0.9;
+        } else if (100000 <= price) {
+            price *= 0.95;
+        }
+        
+        return price;
+
+    }
+}
