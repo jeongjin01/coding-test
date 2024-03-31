8adfe46a1ebb514904a0f6494e440b268bbb6b4c
JeongJin <124764438+jeongjin01@users.noreply.github.com>
2024-03-31 14:52:39 +0900

    [Bronze V] Title: A/B, Time: 220 ms, Memory: 17820 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        double a = sc.nextInt();
+        double b = sc.nextInt();
+        
+        double div = a / b;
+        System.out.println(div);
+    }
+}
