7ade1573af00e7d7c6f39c2c386601ec220ffc5a
JeongJin <124764438+jeongjin01@users.noreply.github.com>
2024-04-26 13:39:01 +0900

    [Bronze III] Title: 최소, 최대, Time: 2424 ms, Memory: 321176 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        
+        for(int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        
+        Arrays.sort(arr);
+		System.out.print(arr[0] + " " + arr[n - 1]);
+    }
+}
